d7c3874ec8d2111917071a19849f28b0212ba86b
RHOOPH <kowshik_achar@yahoo.com>
2022-05-12 16:33:03 +0530

    changed validation to trigger on submit

--- Form/index.js
+++ Form/index.js
@@ -1,14 +1,28 @@
-const validateUserName = (e) => {
-  document.getElementById("validationUserName").innerText = e.value
+const validateUserName = () => {
+  const username = document.getElementById("username").value
+  document.getElementById("validationUserName").innerText = username
     ? ""
     : "Username is required"
+
+  return username
 }
 const validatePassword = (e) => {
-  const isValid = (str) => {
+  const checkIfValid = (str) => {
     if (str.length < 8) return false
     return str.match(/[0-9]/)
   }
-  document.getElementById("validationPassword").innerText = isValid(e.value)
+  const password = document.getElementById("password").value
+  const isValid = checkIfValid(password)
+
+  document.getElementById("validationPassword").innerText = isValid
     ? ""
     : "Password must have 8 characters and at least 1 digit."
+
+  return isValid
+}
+
+const submit = document.getElementById("submit")
+
+submit.onclick = (e) => {
+  if (!(validateUserName() && validatePassword())) e.preventDefault()
 }
