6b56e3b51abfb02deefdd59d7ec1bd2a37d828d1
RHOOPH <kowshik_achar@yahoo.com>
2022-06-04 19:51:27 +0530

    object-hash

--- Node/app.js
+++ Node/app.js
@@ -1,2 +1,9 @@
 const chalk = require("chalk")
+const hash = require("object-hash")
+
 console.log(chalk.red(__dirname))
+
+const arr = [1, 2, 3]
+const arr1 = [1, 2, 3, 4]
+
+console.log(hash(arr) === hash(arr1))
